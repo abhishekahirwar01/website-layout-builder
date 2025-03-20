d8546617e27b82b85c73d9a8c844cf9b4e06c935
Abhishek Ahirwar <ahirwarabhi01@gmail.com>
2025-03-21 00:34:19 +0530

    Fixed GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/website-layout-builder/", // Make sure this matches your repo name
   plugins: [react()],
-  base: "/website-layout-builder/",
 });
